331f3d419bb55f6b1d09b72f111fdcd4c01494a8
Yaroslav1992-art <Lotslav@gmail.com>
2020-01-04 17:40:16 +0200

    Updates

---
+++ precache-manifest.43fff5d16e5737a8c9044e0af33cc535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27862371693bf6b49f2a18f846dc04ad",
+    "url": "/todo-React/index.html"
+  },
+  {
+    "revision": "df5b62d5a787faa45171",
+    "url": "/todo-React/static/css/main.6640f5b3.chunk.css"
+  },
+  {
+    "revision": "32122a7119b9dc47f575",
+    "url": "/todo-React/static/js/2.bb98c239.chunk.js"
+  },
+  {
+    "revision": "6b1e30fbae570017776d1838e82dbe98",
+    "url": "/todo-React/static/js/2.bb98c239.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df5b62d5a787faa45171",
+    "url": "/todo-React/static/js/main.3c726e2e.chunk.js"
+  },
+  {
+    "revision": "8154b192a89d600fac6c",
+    "url": "/todo-React/static/js/runtime-main.3b2e6432.js"
+  }
+]);
